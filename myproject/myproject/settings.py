c3caff8f0abf9e35c01a8a679c180c49c1544907
sekanflor <aschilledeleon06@gmail.com>
2025-02-13 21:27:22 +0800

    fix

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio', 'static')
+]
+

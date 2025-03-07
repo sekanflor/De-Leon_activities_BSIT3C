19513a6a9ba45d970d5cf2f4abbeb8292b4b4802
sekanflor <aschilledeleon06@gmail.com>
2025-03-07 13:36:27 +0800

    doned

--- activity3/activity3/urls.py
+++ activity3/activity3/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  
+
+
+def redirect_to_api(request):
+    return redirect('/api/items/')  
 
 urlpatterns = [
+    path('', redirect_to_api), 
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ✅ I-include ang API URLs
+    path('api/', include('api.urls')),  
 ]

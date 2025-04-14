e45955f5ac6a7dc6910e588645d73b256faa37f3
sekanflor <aschilledeleon06@gmail.com>
2025-04-15 00:30:55 +0800

    activity5

---
+++ activity5/activity5/act5/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Act5Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'act5'
---
+++ activity5/activity5/act5/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=201)
+        return Response(serializer.errors, status=400)
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message': f'Welcome, {request.user.username}!'})
